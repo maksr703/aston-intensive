e2fa51aa4714a20805bc57c9498f705e9fe334e5
Maksim Romanov <maksr703@icloud.com>
2026-06-15 00:25:10 +0300

    fix docker-compose

--- user-service/src/main/java/com/aston/userservice/infrastructure/kafka/config/KafkaConfig.java
+++ user-service/src/main/java/com/aston/userservice/infrastructure/kafka/config/KafkaConfig.java
@@ -1,7 +1,7 @@
 package com.aston.userservice.infrastructure.kafka.config;
 
-
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,20 +17,19 @@ import java.util.Map;
 public class KafkaConfig {
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory() {
+    public ProducerFactory<String, SpecificRecord> producerFactory() {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-
-        props.put("schema.registry.url", "http://localhost:8083");
+        props.put("schema.registry.url", "http://localhost:8081");
 
         return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
+    public KafkaTemplate<String, SpecificRecord> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
