6d8bbeb3462ae435f57c8a68c6b9eff1c7414035
Maksim Romanov <maksr703@icloud.com>
2026-06-14 19:43:24 +0300

    init project

---
+++ notification-service/src/test/java/com/aston/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aston.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
