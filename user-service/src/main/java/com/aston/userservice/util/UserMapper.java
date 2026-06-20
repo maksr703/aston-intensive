4fa78fd7f575ec1ac99142ced08782a1f5ac85d9
Maksim Romanov <rmnv03@icloud.com>
2026-06-20 21:42:25 +0300

    add swager

--- user-service/src/main/java/com/aston/userservice/util/UserMapper.java
+++ user-service/src/main/java/com/aston/userservice/util/UserMapper.java
@@ -7,6 +7,7 @@ public class UserMapper {
 
     public static UserResponse toResponse(User user) {
         return new UserResponse(
+                user.getId(),
                 user.getEmail(),
                 user.getName(),
                 user.getAge()
